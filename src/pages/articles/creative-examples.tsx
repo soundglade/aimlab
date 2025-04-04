ac0e8bccd2eb1e0a36e0ba34857383f2ed7e9821
bitlyfied <bitlyfied@users.noreply.github.com>
2025-04-04 17:45:37 +0300

    fix link to meditation

--- src/pages/articles/creative-examples.tsx
+++ src/pages/articles/creative-examples.tsx
@@ -117,7 +117,7 @@ export default function BlogPost() {
           <ChatbotResponse content={dailyMeditationResponse} truncateAt={200} />
 
           <PlayButtonLine
-            id="hjfwbn6"
+            id="dra65y7"
             title="Awareness and Non-Duality Meditation"
           />
 
