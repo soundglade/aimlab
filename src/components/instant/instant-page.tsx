2665ae57276848774d6a85e2aff32ce5a1f4ced7
bitlyfied <bitlyfied@users.noreply.github.com>
2025-07-25 12:47:37 +0300

    set max 10k chars limit for prompts

--- src/components/instant/instant-page.tsx
+++ src/components/instant/instant-page.tsx
@@ -384,6 +384,7 @@ export default function ReaderPage() {
                 }}
                 disabled={isSubmitting || isDownloading}
                 required
+                maxLength={10000}
                 ref={scriptTextareaRef}
               />
             </div>
